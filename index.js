96af691a3b0d8b1a9e2358416305ffa1383db531
vrsvr <rifatvarisov@gmail.com>
2020-12-29 11:45:01 +0500

    Открытие / закрытие селекта, перенос верстки в js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-import {Select} from './select/select'
-import './select/style.scss'
+import {Select} from './select/select';
+import './select/style.scss';
 
-const select = new Select()
+const select = new Select('#select', {
+  placeholder: 'Выберите элемент',
+  data: [
+    {id: '1', value: 'React'},
+    {id: '2', value: 'Angular'},
+    {id: '3', value: 'Vue'},
+    {id: '4', value: 'React Native'},
+    {id: '5', value: 'Next'},
+    {id: '6', value: 'Nest'},
+  ]
+});
+
+window.s = select;
--- select/select.js
+++ select/select.js
@@ -1,3 +1,73 @@
+const getTemplate = (data = [], placeholder) => {
+  const text = placeholder ?? 'Placeholder не задан'
+  const items = data.map(item => {
+    return `
+      <li class="select__item">${item.value}</li>
+    `
+  });
+
+  return `
+    <div class="select__input" data-type="input">
+      <span>${text}</span>
+      <i class="fa fa-chevron-down" data-type="arrow"></i>
+    </div>
+    <div class="select__dropdown">
+      <ul class="select__list">
+        ${items.join('')}
+      </ul>
+    </div>
+  `
+};
+
 export class Select {
-  
+  constructor(selector, options) {
+    this.$el = document.querySelector(selector);
+    this.options = options;
+
+    this.#render();
+    this.#setup();
+  }
+
+  #render() {
+    const {placeholder, data} = this.options;
+    this.$el.classList.add('select');
+    this.$el.innerHTML = getTemplate(data, placeholder);
+  }
+
+  #setup() {
+    this.clickHandler = this.clickHandler.bind(this);
+    this.$el.addEventListener('click', this.clickHandler);
+    this.$arrow = this.$el.querySelector('[data-type="arrow"]');
+  }
+
+  clickHandler(event) {
+    const {type} = event.target.dataset;
+    if (type === 'input') {
+      this.toggle();
+    }
+  }
+
+  get isOpen() {
+    return this.$el.classList.contains('open')
+  }
+
+  toggle() {
+    this.isOpen ? this.close() : this.open();
+  }
+
+  open() {
+    this.$el.classList.add('open');
+    this.$arrow.classList.remove('fa-chevron-down');
+    this.$arrow.classList.add('fa-chevron-up');
+  }
+
+  close() {
+    this.$el.classList.remove('open');
+    this.$arrow.classList.add('fa-chevron-down');
+    this.$arrow.classList.remove('fa-chevron-up');
+  }
+
+  destroy() {
+    this.$el.removeEventListener('click', this.clickHandler);
+  }
 }
